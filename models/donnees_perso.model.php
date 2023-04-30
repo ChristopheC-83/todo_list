174795a196e70571b16d6ebccdf7c4fd9892379e
ChristopheC83 <christophe.chiappetta@gmail.com>
2023-04-30 14:29:03 +0200

    initialisation

---
+++ models/donnees_perso.model.php
@@ -0,0 +1,13 @@
+<?php
+
+define("mysql","89.116.147.103" );
+define("dbname","u256533777_todolist" );
+define("user","u256533777_userTodoList" );
+define("mdpbd","Santa30420*" );
+
+// $msql = "89.116.147.103";
+// $dbname = "u256533777_todolist";
+// $user = "u256533777_userTodoList";
+// $mdpbd ="Santa30420*";
+
+?>
