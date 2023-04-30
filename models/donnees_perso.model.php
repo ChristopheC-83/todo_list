0685b15bbb50129bb6a227ccbc3031dcd0b1dea5
ChristopheC83 <christophe.chiappetta@gmail.com>
2023-04-30 14:29:49 +0200

    ok ?

--- models/donnees_perso.model.php
+++ models/donnees_perso.model.php
@@ -5,9 +5,6 @@ define("dbname","u256533777_todolist" );
 define("user","u256533777_userTodoList" );
 define("mdpbd","Santa30420*" );
 
-// $msql = "89.116.147.103";
-// $dbname = "u256533777_todolist";
-// $user = "u256533777_userTodoList";
-// $mdpbd ="Santa30420*";
+
 
 ?>
