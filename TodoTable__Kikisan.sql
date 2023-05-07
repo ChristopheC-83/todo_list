a12a2a5cf7f8ec4ad39ae74db0f65b0ceb2ec6ae
ChristopheC83 <christophe.chiappetta@gmail.com>
2023-05-07 19:05:03 +0200

    finish avec sql

---
+++ TodoTable__Kikisan.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : dim. 07 mai 2023 à 17:03
+-- Version du serveur : 10.6.12-MariaDB-cll-lve
+-- Version de PHP : 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `u256533777_todolist`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `TodoTable__Kikisan`
+--
+-- Erreur de lecture de structure pour la table u256533777_todolist.TodoTable__Kikisan : #1146 - La table 'u256533777_todolist.TodoTable__Kikisan' n'existe pas
+-- Erreur de lecture des données pour la table u256533777_todolist.TodoTable__Kikisan : #1064 - Erreur de syntaxe près de 'FROM `u256533777_todolist`.`TodoTable__Kikisan`' à la ligne 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
